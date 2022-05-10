c9da3377581e7cc5f245012066deec7e452e1f8d
Ayush Khare <ayushkhare@FVFGV64GQ05N.local>
2022-05-10 14:33:45 +0530

    send mail working

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,80 @@
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
-        sendErrorMailToSPOC();
+    public static void main(String[] args) throws MessagingException, IOException {
+        EmailTrigger mail = new EmailTrigger();
+        mail.setupServerProperties();
+        mail.draftEmail();
+        mail.sendEmail();
     }
 
-    private static void sendErrorMailToSPOC() {
-        final String SENDER_EMAIL = "project.0.dummy.0@gmail.com"; // VM's name
-        final String RECEIVER_EMAIL = "ayushkhare111@gmail.com";
-        final String HOST = "localhost"; // can be replaced VM's external IP
-        final String PORT = "8002"; // Vm's port
-        final String PASSWORD = "jaadu123";
+    static class EmailTrigger {
+        Session newSession = null;
+        MimeMessage mimeMessage = null;
 
-        final String SUBJECT = "No Data found";
-        final String BODY_TEXT = "The Source repo is not giving any data. Please look into that.";
+        private final String SENDER_EMAIL = "project.0.dummy.0@gmail.com";
+        private final String SENDER_EMAIL_PASSWD = "jaadu123";
 
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", HOST);
-        properties.setProperty("mail.smtp.port", PORT);
-        properties.setProperty("mail.smtp.auth", "true");
-        properties.setProperty("mail.smtp.timeout", "60000");
-        properties.setProperty("mail.smtp.starttls.enable", "true");
-        properties.setProperty("mail.user", SENDER_EMAIL);
-        properties.setProperty("mail.password", PASSWORD);
+        /**
+         * Establishes smtp connection from provided host and email credentials
+         * Sends the message to all the recipients provided
+         *
+         * @throws MessagingException
+         */
+        private void sendEmail() throws MessagingException {
+            String emailHost = "smtp.gmail.com";
 
-        Session session = Session.getDefaultInstance(properties, new Authenticator() {
-            @Override
-            public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(SENDER_EMAIL, PASSWORD);
+            Transport transport = newSession.getTransport("smtp");
+            transport.connect(emailHost, SENDER_EMAIL, SENDER_EMAIL_PASSWD);
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            transport.close();
+
+            System.out.println("Email successfully sent !!!");
+        }
+
+        /**
+         * This method drafts an email with subject, body and
+         * adds all the recipients mentioned
+         *
+         * @throws MessagingException
+         */
+        private void draftEmail() throws MessagingException {
+            String[] emailRecipients = {"ayush.khare@searce.com", "shreyas.patel@searce.com"};
+            String emailSubject = "No data found";
+            String emailBody = "The source repository is not giving any data. " +
+                    "Eventually query results are empty, hence there are no results for the kpi search in the enterprise search portal. " +
+                    "Please look into that.";
+
+            mimeMessage = new MimeMessage(newSession);
+
+            for (String emailRecipient : emailRecipients) {
+                mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(emailRecipient));
             }
-        });
 
-        try {
-            MimeMessage msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress(SENDER_EMAIL));
-            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(RECEIVER_EMAIL));
+            mimeMessage.setSubject(emailSubject);
+            mimeMessage.setText(emailBody);
+        }
 
-            msg.setSubject(SUBJECT);
-            msg.setText(BODY_TEXT);
+        /**
+         * This method sets-up smtp essential properties
+         * Session authentication via email password of the sender
+         */
+        private void setupServerProperties() {
+            Properties properties = System.getProperties();
+            properties.put("mail.smtp.port", "587");
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.starttls.enable", "true");
 
-            Transport.send(msg);
-
-            System.out.println("Mail Sent");
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            newSession = Session.getDefaultInstance(properties,new Authenticator() {
+                @Override
+                public PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(SENDER_EMAIL, SENDER_EMAIL_PASSWD);
+                }
+            });
         }
     }
 }
